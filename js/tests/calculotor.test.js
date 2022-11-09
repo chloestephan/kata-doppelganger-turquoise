072b35d224ad6d355880e60d52f40f84b9f36928
Vincent Petit <vincent.petit@efrei.net>
2022-11-09 16:48:54 +0100

    commit

--- js/tests/calculotor.test.js
+++ js/tests/calculotor.test.js
@@ -1,7 +1,32 @@
-const test = require('tape')
+const test = require("tape");
+const Calculator = require("../calculator");
 
-test('should throw when not authorized', (t) => {
-  // TODO: write a test that fails due to the bug in
-  // Calculator.divide()
-  t.end()
-})
+test("should throw when not authorized", (t) => {
+	// TODO: write a test that fails due to the bug in
+	// Calculator.divide()
+
+	// Should fail because calculator.divide() will not work when the authorizer is true.
+
+	// Given
+	const param = new Object();
+	param.authorize = () => false;
+
+	const awaitedResult = new Error("Not authorized");
+
+	// When
+	const calculator = new Calculator(param);
+	const result = calculator.divide(4, 2);
+
+	// Then
+	t.equal(result, awaitedResult);
+
+	t.end();
+	/*
+  Failed Tests: There was 1 failure should throw when not authorized × should be strictly equal
+
+  total:     1
+  passing:   0
+  failing:   1
+  duration:  237ms
+  */
+});
--- js/tests/discount-applier.test.js
+++ js/tests/discount-applier.test.js
@@ -1,13 +1,52 @@
-const test = require('tape')
+const test = require("tape");
+const discountApplier = require("../discount-applier");
 
-test('apply v1', (t) => {
-  // TODO: write a test that fails due to the bug in
-  // DiscountApplier.applyV1
-  t.end()
-})
+test("should throws an error in the loop", (t) => {
+	// TODO: write a test that fails due to the bug in
+	// DiscountApplier.applyV1
+	//given
 
-test('apply v2', (t) => {
-  // TODO: write a test that fails due to the bug in
-  // DiscountApplier.applyV2
-  t.end()
-})
+	const awaitedresult = new Error("Initial loop cp wrong");
+	var notifier = {
+		user: "",
+		message: "",
+		notify: (user, message) => console.log(user + " : " + message),
+	};
+	const tab = ["Tom", "Nike", "Gean", "Worf", "Shield"];
+	const discount = 10;
+
+	//When
+	const discountApp = new discountApplier(notifier);
+	const result = discountApp.applyV1(discount, tab);
+
+	class testV1 {
+		testV1fn(discount, users) {
+			console.log("Here are what we expected \n");
+			//let i = 0 to get expected results
+			for (let i = 1; i < users.length; i++) {
+				const message = `You've got a new discount of ${discount}`;
+				const user = users[0];
+				console.log(user + " : " + message);
+			}
+		}
+	}
+
+	const testv1 = new testV1();
+	const expected = testv1.testV1fn(discount, tab);
+
+	//Then
+
+	t.equal(result, expected);
+	t.end();
+});
+
+test("apply v2", (t) => {
+	// TODO: write a test that fails due to the bug in
+	// DiscountApplier.
+	//given
+
+	//When
+
+	//Then
+	t.end();
+});
